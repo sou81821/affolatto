059f75f543f40cac960e536577579abafdf8b881
Sou Konishi <srm81821@gmail.com>
2016-10-22 15:09:43 +0900

    default画像表示・facebookボタン追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   has_many :tweets
 
-  has_attached_file :avatar, styles:  { medium: "300x300#", thumb: "100x100#" }
+  has_attached_file :avatar,
+                    :styles => { medium: "300x300#", thumb: "100x100#" }
   validates_attachment_content_type :avatar, content_type: ["image/jpg", "image/jpeg", "image/png"]
 
   def self.find_for_oauth(auth)

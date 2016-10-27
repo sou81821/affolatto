6025931b786829efcc4bd4fa44cacac2b631894d
Sou Konishi <srm81821@gmail.com>
2016-10-27 19:39:04 +0900

    現在地に合わせた地図表示(仮)

--- app/assets/javascripts/function.js
+++ app/assets/javascripts/function.js
@@ -16,13 +16,47 @@ $(function(){
 });
 
 
+// 現在地に合わせた地図表示
 function successFunc(position) {
   console.log( position.coords.latitude );
   console.log( position.coords.longitude );
+
+  handler = Gmaps.build('Google');
+  handler.buildMap(
+    {
+      provider: {
+        zoom: 16,
+        center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+      },
+      internal: {id: 'map'}
+    },
+    function(){
+      markers = handler.addMarkers(<%=raw @hash.to_json %>);
+      handler.bounds.extendWith(markers);
+      // handler.fitMapToBounds();
+    }
+  );
 }
 
+// 現在地の取得に失敗したとき，デフォルトの地図表示
 function errorFunc(error) {
   console.log( error.code );
+
+  handler = Gmaps.build('Google');
+  handler.buildMap(
+    {
+      provider: {
+        zoom: 16,
+        center: new google.maps.LatLng(34.6945, 135.195)
+      },
+      internal: {id: 'map'}
+    },
+    function(){
+      markers = handler.addMarkers(<%=raw @hash.to_json %>);
+      handler.bounds.extendWith(markers);
+      // handler.fitMapToBounds();
+    }
+  );
 }
 
 var optionObj = {

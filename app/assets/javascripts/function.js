36f7a49cacf49fa7fd0d498e3d666c19fc071f72
Sou Konishi <srm81821@gmail.com>
2016-11-03 00:35:51 +0900

    change js

--- app/assets/javascripts/function.js
+++ app/assets/javascripts/function.js
@@ -1,13 +1,28 @@
 // サイドメニュー開閉処理
+// $(function () {
+//   var $body = $('body');
+//   $('#toggle').on('click', function () {
+//     // $('.side-menu').show()
+//     $body.toggleClass('side-open');
+//   });
+//
+//   $('#js__overlay').on('click', function () {
+//     $body.toggleClass('side-open');
+//     // $('.side-menu').hide()
+//   });
+// });
+
+
 $(function () {
   var $body = $('body');
   $('#toggle').on('click', function () {
-    // $('.side-menu').show()
+    $('.side-menu').show()
     $body.toggleClass('side-open');
   });
-
   $('#js__overlay').on('click', function () {
-    $body.toggleClass('side-open');
-    // $('.side-menu').hide()
+      $body.removeClass('side-open');
+  });
+  $('.side-open').on('webkitAnimationEnd', function(){
+        $('.side-menu').hide()
   });
 });

472590fe65e6a08075ffa318401e8448a907969f
Sou Konishi <srm81821@gmail.com>
2016-11-03 01:29:37 +0900

    change js

--- app/assets/javascripts/function.js
+++ app/assets/javascripts/function.js
@@ -12,17 +12,14 @@
 //   });
 // });
 
-
-$(function () {
-  var $body = $('body');
-  $('#toggle').on('click', function () {
-    $('.side-menu').show()
-    $body.toggleClass('side-open');
-  });
-  $('#js__overlay').on('click', function () {
-      $body.removeClass('side-open');
-  });
-  $('.wrapper').on('webkitAnimationEnd', function(){
-      $('.side-menu').hide()
-  });
+var $body = $('body');
+$('#toggle').on('click', function () {
+  $('.side-menu').show()
+  $body.toggleClass('side-open');
+});
+$('#js__overlay').on('click', function () {
+    $body.removeClass('side-open');
+});
+$('.wrapper').on('transitionend', function(){
+    $('.side-menu').hide()
 });

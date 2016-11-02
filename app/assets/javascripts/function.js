c1de61f7ba19d733e7dd2bcfc3841a2a4a81d701
Sou Konishi <srm81821@gmail.com>
2016-11-03 05:13:17 +0900

    スライドメニュー

--- app/assets/javascripts/function.js
+++ app/assets/javascripts/function.js
@@ -1,32 +1,4 @@
 // サイドメニュー開閉処理
-// $(function () {
-//   var $body = $('body');
-//   $('#toggle').on('click', function () {
-//     // $('.side-menu').show()
-//     $body.toggleClass('side-open');
-//   });
-//
-//   $('#js__overlay').on('click', function () {
-//     $body.toggleClass('side-open');
-//     // $('.side-menu').hide()
-//   });
-// });
-
-// $(function (){
-//   var $body = $('body');
-//   $('#toggle').on('click', function () {
-//     $('.side-menu').show();
-//     $body.toggleClass('side-open');
-//   });
-//   $('#js__overlay').on('click', function () {
-//     $('.side-open.wrapper').on('transitionend', function(){
-//         $('.side-menu').hide();
-//     });
-//     $body.toggleClass('side-open');
-//   });
-// });
-
-
 $(function (){
   var $body = $('body');
   $('#toggle').on('click', function () {
@@ -38,7 +10,6 @@ $(function (){
   });
   $('.wrapper').on('transitionend', function(){
     if(!($body.hasClass('side-open'))){
-      // console.log("a");
       $('.side-menu').hide();
     }
   });

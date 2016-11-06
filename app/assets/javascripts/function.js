9b8dd6ef4bb6752b7fffbb110ec1b90ecdfd56ef
Sou Konishi <srm81821@gmail.com>
2016-11-07 04:44:32 +0900

    css

--- app/assets/javascripts/function.js
+++ app/assets/javascripts/function.js
@@ -3,9 +3,11 @@ $(function (){
   var $body = $('body');
   $('#toggle').on('click', function () {
     $('.side-menu').show();
+    $('#header-center').hide();
     $body.toggleClass('side-open');
   });
   $('#js__overlay').on('click', function () {
+    $('#header-center').show();
     $body.toggleClass('side-open');
   });
   $('.wrapper').on('transitionend', function(){
